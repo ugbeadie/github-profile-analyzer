d138f53df867d61363a908317f1bba433f675dd0
ugbeadie <ugbeadie3@gmail.com>
2022-09-27 16:31:17 +0100

    add ScrollToTop when switching routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home/Home';
 import Profile from './components/pages/Profile/Profile'
-
+import ScrollToTop from './components/ScrollToTop'
 function App() {
   return (
     <>
       <Router>
+      <ScrollToTop />
         <Navbar/>
         <div className='content'>          
           <Routes>
